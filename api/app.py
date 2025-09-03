f9082b74695b8e4e4ad7462550206cc80f7818f0
grf <tuprofe101@gmail.com>
2025-09-03 15:33:54 +0200

    dynamic part

--- api/app.py
+++ api/app.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://api.grfone.es"],
+    allow_origins=["*"],  # https://grfone.es/types_of_clouds.html
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
