c22237c603f5126a6f2fcd94e9cda16d3871d768
grf <tuprofe101@gmail.com>
2025-09-03 10:49:49 +0200

    static webpage

--- api/app.py
+++ api/app.py
@@ -11,7 +11,7 @@ app = FastAPI()
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://www.grfone.es"],
+    allow_origins=["https://api.grfone.es"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
